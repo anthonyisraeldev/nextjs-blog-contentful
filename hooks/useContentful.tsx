84733d63d4dce64dd2a656de4448a90e4c740851
anthonyisraeldevv <anthonyisrael.dev@gmail.com>
2024-10-24 12:41:00 -0300

    updated

--- hooks/useContentful.tsx
+++ hooks/useContentful.tsx
@@ -5,7 +5,16 @@ import useSWR from "swr";
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const GetContentfulPosts = () => {
-  const { data, error } = useSWR("/api/contentful-entries", fetcher);
+  const { data, error } = useSWR("/api/contentful-entries", fetcher, {
+    refreshInterval: 5000,
+  });
+
+  // export const GetContentfulPosts = () => {
+  // const { data, error } = useSWR("/api/contentful-entries", fetcher, {
+  //   revalidateOnFocus: true, // Revalida al enfocar la ventana
+  //   refreshInterval: 10000,  // Revalida automáticamente cada 10 segundos
+  // });
+
   return {
     data,
     isLoading: !error && !data,
