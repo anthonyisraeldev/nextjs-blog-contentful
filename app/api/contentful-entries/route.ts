c85a405a683db1d91198a5a9532a844041a8ebf3
anthonyisraeldevv <anthonyisrael.dev@gmail.com>
2024-10-24 09:40:34 -0300

    ISR

--- app/api/contentful-entries/route.ts
+++ app/api/contentful-entries/route.ts
@@ -13,3 +13,26 @@ export async function GET() {
     );
   }
 }
+
+// api/contentful-entries/route.ts
+
+// export async function GET() {
+//   try {
+//     const entries = await fetchEntries("blog");
+
+//     // Configura los encabezados para la revalidación
+//     const response = NextResponse.json(entries);
+//     response.headers.set(
+//       "Cache-Control",
+//       "public, s-maxage=60, stale-while-revalidate=30"
+//     );
+
+//     return response;
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json(
+//       { error: "Error fetching Contentful entries" },
+//       { status: 500 }
+//     );
+//   }
+// }
--- hooks/useContentful.tsx
+++ hooks/useContentful.tsx
@@ -4,8 +4,14 @@ import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
+// export const GetContentfulPosts = () => {
+//   const { data, error } = useSWR("/api/contentful-entries", fetcher);
+
 export const GetContentfulPosts = () => {
-  const { data, error } = useSWR("/api/contentful-entries", fetcher);
+  const { data, error } = useSWR("/api/contentful-entries", fetcher, {
+    revalidateOnFocus: true, // Revalida al enfocar la ventana
+    refreshInterval: 10000, // Revalida automáticamente cada 10 segundos
+  });
 
   return {
     data,
