0150d5e90fd38447222b87a07952686fa42e07d9
anthonyisraeldevv <anthonyisrael.dev@gmail.com>
2024-10-24 12:04:15 -0300

    Test ravalidation

--- app/api/contentful-entries/route.ts
+++ app/api/contentful-entries/route.ts
@@ -1,9 +1,9 @@
 import { fetchEntries } from "@/lib/client";
-// import { revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // revalidatePath("/", "layout");
+  revalidatePath("/", "layout");
   try {
     const entries = await fetchEntries("blog");
     return NextResponse.json(entries);
