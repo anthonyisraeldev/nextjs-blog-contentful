7a4cc98b6b70e96e9283785c4917245a11d19d4b
anthonyisraeldevv <anthonyisrael.dev@gmail.com>
2024-10-24 09:45:30 -0300

    Test ravalidation

--- app/api/contentful-entries/route.ts
+++ app/api/contentful-entries/route.ts
@@ -1,33 +1,10 @@
 import { fetchEntries } from "@/lib/client";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  try {
-    const entries = await fetchEntries("blog");
-    return NextResponse.json(entries);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { error: "Error fetching Contentful entries" },
-      { status: 500 }
-    );
-  }
-}
-
-// api/contentful-entries/route.ts
-
 // export async function GET() {
 //   try {
 //     const entries = await fetchEntries("blog");
-
-//     // Configura los encabezados para la revalidación
-//     const response = NextResponse.json(entries);
-//     response.headers.set(
-//       "Cache-Control",
-//       "public, s-maxage=60, stale-while-revalidate=30"
-//     );
-
-//     return response;
+//     return NextResponse.json(entries);
 //   } catch (error) {
 //     console.log(error);
 //     return NextResponse.json(
@@ -36,3 +13,24 @@ export async function GET() {
 //     );
 //   }
 // }
+
+export async function GET() {
+  try {
+    const entries = await fetchEntries("blog");
+
+    // Configura los encabezados para la revalidación
+    const response = NextResponse.json(entries);
+    response.headers.set(
+      "Cache-Control",
+      "public, s-maxage=10, stale-while-revalidate=15"
+    );
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Error fetching Contentful entries" },
+      { status: 500 }
+    );
+  }
+}
--- hooks/useContentful.tsx
+++ hooks/useContentful.tsx
@@ -4,14 +4,14 @@ import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-// export const GetContentfulPosts = () => {
-//   const { data, error } = useSWR("/api/contentful-entries", fetcher);
-
 export const GetContentfulPosts = () => {
-  const { data, error } = useSWR("/api/contentful-entries", fetcher, {
-    revalidateOnFocus: true, // Revalida al enfocar la ventana
-    refreshInterval: 10000, // Revalida automáticamente cada 10 segundos
-  });
+  const { data, error } = useSWR("/api/contentful-entries", fetcher);
+
+  // export const GetContentfulPosts = () => {
+  // const { data, error } = useSWR("/api/contentful-entries", fetcher, {
+  //   revalidateOnFocus: true, // Revalida al enfocar la ventana
+  //   refreshInterval: 10000,  // Revalida automáticamente cada 10 segundos
+  // });
 
   return {
     data,
